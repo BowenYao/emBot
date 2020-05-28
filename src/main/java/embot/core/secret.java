cd6fb226519ed122590578fbd95cf92f2b8f14f0
BowenYao <boboarrows@gmail.com>
2020-05-28 01:06:02 -0400

    Removed ServerInits.ser

--- src/main/java/embot/core/secret.java
+++
@@ -1,7 +0,0 @@
-package embot.core;
-
-public class secret {
-    //Class for accessing secret info. Right now it's just the discord bot token. Could expand for other apis and the like
-    private static final String discordToken = "token here";
-    public static String getDiscordToken(){return discordToken;}
-}
