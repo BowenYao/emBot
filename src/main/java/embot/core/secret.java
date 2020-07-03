5d8547283d72bfee86fc490a18964801a69f855b
BowenYao <boboarrows@gmail.com>
2020-07-02 21:23:43 -0400

    Changed the way secrets works

--- src/main/java/embot/core/secret.java
+++ src/main/java/embot/core/secret.java
@@ -1,7 +1,33 @@
 package embot.core;
 
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.*;
+import java.util.HashMap;
+
 public class secret {
     //Class for accessing secret info. Right now it's just the discord bot token. Could expand for other apis and the like
-    private static final String discordToken = "your token here";
-    public static String getDiscordToken(){return discordToken;}
+    private static final HashMap<String, String> secrets = new HashMap<>(1);
+
+    public static String getDiscordToken() {
+        return secrets.get("discord_token");
+    }
+
+    static {
+        File secret = new File("secret.json");
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(secret);
+            JsonReader jsonReader = new JsonReader(new InputStreamReader(fis));
+            jsonReader.beginObject();
+            while (jsonReader.hasNext()) {
+                String name = jsonReader.nextName();
+                secrets.put(name, jsonReader.nextString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
